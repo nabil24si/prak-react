4387bc794e92386608ccbeeec3d7d49267db044e
nabil2sib <nabil24si@mahasiswa.pcr.ac.id>
2026-04-07 16:28:45 +0700

    Pertemuan 4 Penambahan Best Practie State dan Grid

--- src/pertemuan-4/main.jsx
+++ src/pertemuan-4/main.jsx
@@ -1,12 +1,13 @@
 import { createRoot } from "react-dom/client";
-
+import ResponsiveDesig from "./ResponsiveDesign";
 import './tailwind.css';
 import FrameworkListSearchFilter from "./FrameworklistSearchFilter";
 
 createRoot(document.getElementById("root"))
     .render(
         <div>
-            <FrameworkListSearchFilter/>
+            <ResponsiveDesig/>
+            {/* <FrameworkListSearchFilter/> */}
 
         </div>
     )
