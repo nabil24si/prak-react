e5e03255a893c2e80114ffc64ffe06c558cab936
nabil2sib <nabil24si@mahasiswa.pcr.ac.id>
2026-04-28 15:12:25 +0700

    Pertemuan 7  Lazy Loading + Suspense

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,33 @@
-import { useState } from 'react'
+import React, { Suspense, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import Dashboard from './pages/Dashnoard';
+const Dashboard = React.lazy(() => import("./pages/Dashnoard"))
 import './assets/tailwind.css';
 import { Route, Routes } from 'react-router-dom';
-import Customers from './pages/Customers';
-import Orders from './pages/Orders';
-import NotFound from './pages/NotFound';
-import MainLayout from './layouts/MainLayout';
-import AuthLayout from './layouts/AuthLayout';
-import Login from './pages/auth/Login';
-import Register from './pages/auth/Register';
-import Forgot from './pages/auth/Forget';
+import Loading from './components/Loading';
+// import Customers from './pages/Customers';
+const Customers = React.lazy(() => import("./pages/Customers"))
+// import Orders from './pages/Orders';
+const Orders = React.lazy(() => import("./pages/Orders"))
+// import NotFound from './pages/NotFound';
+const NotFound = React.lazy(() => import("./pages/NotFound"))
+// import MainLayout from './layouts/MainLayout';
+const MainLayout = React.lazy(() => import("./layouts/MainLayout"))
+// import AuthLayout from './layouts/AuthLayout';
+const AuthLayout = React.lazy(() => import("./layouts/AuthLayout"))
+// import Login from './pages/auth/Login';
+const Login = React.lazy(() => import("./pages/auth/Login"))
+// import Register from './pages/auth/Register';
+const Register = React.lazy(() => import("./pages/auth/Register"))
+// import Forgot from './pages/auth/Forget';
+const Forgot = React.lazy(() => import("./pages/auth/Forget"))
+
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
+    <Suspense fallback={<Loading />}>
     <Routes>
         <Route element={<MainLayout/>}>
         <Route path="*" element={<NotFound />} />
@@ -34,6 +45,7 @@ function App() {
             <Route path="/forgot" element={<Forgot/>} />
         </Route>
     </Routes>
+    </Suspense>
   )
 }
 
